02e181a0d341c9f869f3e4ca58835d1dba62b665
jessicard <jessicard@github.com>
2017-02-09 15:22:08 -0800

    Manually truncate

--- twitter_account.rb
+++ twitter_account.rb
@@ -6,6 +6,7 @@ class TwitterAccount
   CONVERTED_LINK_LENGTH = 22
   MAX_TWEET_LENGTH = 140
   SPACE_LENGTH = 1
+  ELLIPSIS_LENGTH = 1
 
   def initialize
     @client = Twitter::REST::Client.new do |config|
@@ -24,28 +25,10 @@ class TwitterAccount
     tweet_length = title.length + CONVERTED_LINK_LENGTH + SPACE_LENGTH
     truncated_title = title
 
-    if tweet_length > MAX_TWEET_LENGTH
-      difference = (tweet_length - MAX_TWEET_LENGTH) + (CONVERTED_LINK_LENGTH + SPACE_LENGTH)
-
-      truncated_title = title.truncate(MAX_TWEET_LENGTH - difference)
+    if title.length > (MAX_TWEET_LENGTH - (CONVERTED_LINK_LENGTH + SPACE_LENGTH))
+      truncated_title = "#{title[0, (MAX_TWEET_LENGTH - (CONVERTED_LINK_LENGTH + SPACE_LENGTH + ELLIPSIS_LENGTH))]}…"
     end
 
     "#{truncated_title} #{link}"
-  end
-end
-
-class String
-  def truncate(truncate_at, options = {})
-    return dup unless length > truncate_at
-
-    options[:omission] ||= '...'
-    length_with_room_for_omission = truncate_at - options[:omission].length
-    stop = if options[:separator]
-        rindex(options[:separator], length_with_room_for_omission) || length_with_room_for_omission
-      else
-        length_with_room_for_omission
-      end
-
-    "#{self[0...stop]}#{options[:omission]}"
   end
 end
