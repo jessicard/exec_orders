69e14bce85f6623eb510fdbc65640725b06bb733
jessicard <jessicard@github.com>
2017-02-09 15:29:12 -0800

    Only one loop necessary

--- backfill.rb
+++ backfill.rb
@@ -3,12 +3,7 @@ require "./twitter_account"
 
 twitter_account = TwitterAccount.new
 api_results = FederalRegister.backfill["results"]
-backfill_tweet_info = []
 
 api_results.each do |result|
-  backfill_tweet_info << { title: result["title"], link: result["html_url"] }
-end
-
-backfill_tweet_info.each do |info|
-  twitter_account.tweet(TwitterAccount.truncate_content(info[:title], info[:link]))
+  twitter_account.tweet(TwitterAccount.truncate_content(result["title"], result["html_url"]))
 end
