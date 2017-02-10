bb9d2f1fb03357b4272e26ff2749f79cb6974df5
jessicard <jessicard@github.com>
2017-02-09 16:28:31 -0800

    Use oldest order

--- federal_register.rb
+++ federal_register.rb
@@ -4,7 +4,7 @@ require "pp"
 
 class FederalRegister
   def self.executive_orders_from(date)
-    res = Faraday.get("https://www.federalregister.gov/api/v1/documents.json?per_page=50&order=newest&conditions%5Bpublication_date%5D%5Bgte%5D=#{date}&conditions%5Bpresidential_document_type%5D%5B%5D=executive_order")
+    res = Faraday.get("https://www.federalregister.gov/api/v1/documents.json?per_page=50&order=oldest&conditions%5Bpublication_date%5D%5Bgte%5D=#{date}&conditions%5Bpresidential_document_type%5D%5B%5D=executive_order")
 
     JSON.parse(res.body)
   end
