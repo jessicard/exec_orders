1075c1ec1fdb20e4f20538dc0397e58192b87e79
Jake Boxer <jake@github.com>
2017-02-09 16:41:26 -0800

    Bring back ARGV

--- backfill.rb
+++ backfill.rb
@@ -1,7 +1,7 @@
 require "./federal_register"
 require "./executive_order_processor"
 
-processor = ExecutiveOrderProcessor.new
+processor = ExecutiveOrderProcessor.new(ARGV.first.to_sym)
 
 FederalRegister.backfill.each do |executive_order|
   processor.process(executive_order)
