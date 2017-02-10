c5d3bcc34d6a3d7b3b53f6e9ca0ea4af3170c3f9
jessicard <jessicard@github.com>
2017-02-09 16:26:32 -0800

    Tweet instead of printing

--- process_orders.rb
+++ process_orders.rb
@@ -4,11 +4,12 @@ require "pp"
 require "./federal_register"
 require "./twitter_account"
 
+twitter_account = TwitterAccount.new
 todays_date = Time.now.strftime("%Y-%m-%d")
 api_results = FederalRegister.executive_orders_from(todays_date)["results"]
 
 if api_results
   api_results.each do |result|
-    pp TwitterAccount.truncate_content(result["title"], result["html_url"])
+    twitter_account.tweet(TwitterAccount.truncate_content(result["title"], result["html_url"]))
   end
 end
