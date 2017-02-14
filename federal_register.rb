f07e5a4b37840a4246ebb8850c1ad85b40a5e07d
jessicard <jessicard@github.com>
2017-02-14 10:26:06 -0800

    Fix buggies

--- federal_register.rb
+++ federal_register.rb
@@ -6,18 +6,18 @@ class FederalRegister
   def self.executive_orders_from(date)
     res = Faraday.get("https://www.federalregister.gov/api/v1/documents.json?per_page=50&order=oldest&conditions%5Bpublication_date%5D%5Bgte%5D=#{date}&conditions%5Bpresidential_document_type%5D%5B%5D=executive_order")
 
-    executive_orders_from(res)
+    create_executive_orders_from(res)
   end
 
   def self.backfill
     res = Faraday.get("https://www.federalregister.gov/api/v1/documents.json?per_page=50&order=oldest&conditions%5Bpublication_date%5D%5Bgte%5D=2017-01-24&conditions%5Bpresidential_document_type%5D%5B%5D=executive_order")
 
-    executive_orders_from(res)
+    create_executive_orders_from(res)
   end
 
   private
 
-  def self.executive_orders_from(response)
+  def self.create_executive_orders_from(response)
     results = JSON.parse(response.body)["results"]
     results.map { |eo| ExecutiveOrder.from_federal_register_hash(eo) }
   end
--- process_executive_orders_from_today.rb
+++ process_executive_orders_from_today.rb
@@ -1,7 +1,8 @@
 require "./federal_register"
 require "./twitter_account"
+require "./executive_order_processor"
 
-processor = ExecutiveOrderProcessor.new(:twitter)
+processor = ExecutiveOrderProcessor.new
 todays_date = Time.now.strftime("%Y-%m-%d")
 
 FederalRegister.executive_orders_from(todays_date).each do |order|
